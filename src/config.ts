08ef78ee91eb12ab7150b18949305b8861cdd274
miikeeee <49447434+miikeeee@users.noreply.github.com>
2025-05-26 19:25:23 +0200

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { ConfigType } from "@/types/ConfigType";
+
+export const config: ConfigType = {
+  logo: "/logo.png",
+  farbe: "#0ea5e9",
+  anrede: "Herr/Frau Mustermann",
+  leadEmail: "info@immobilienbuero.de",
+  telefon: "0221-12345678",
+  adresse: "Musterstraße 1, 12345 Musterstadt",
+  officeStreet: "Musterstraße 1",
+  officeZip: "12345",
+  officeCity: "Musterstadt"
+};
