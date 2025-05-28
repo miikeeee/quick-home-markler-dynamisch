9e71e33f8d457bece7c55861b4945cbedfd79dce
miikeeee <49447434+miikeeee@users.noreply.github.com>
2025-05-28 17:34:12 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,7 @@ export const config: ConfigType = {
   logo: "/logo.png",
   farbe: "#0ea5e9",
   anrede: "Herr/Frau Mustermann",
-  leadEmail: "info@immobilienbuero.de",
-  telefon: "0221-12345678",
-  adresse: "Musterstraße 1, 12345 Musterstadt",
-  officeStreet: "Musterstraße 1",
-  officeZip: "12345",
-  officeCity: "Musterstadt"
+  bueroStrasse: "Siebengebirgsstr. 59",
+  bueroPLZ: "53639",                   
+  bueroStadt: "Königswinter"           
 };
